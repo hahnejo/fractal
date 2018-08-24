433a2168205b19c8a85c231c5e514626a55261fd
Hahn Jo <hjo@e1z3r2p6.42.us.org>
2018-08-24 15:17:38 -0700

    complete

--- include/fractal.h
+++ include/fractal.h
@@ -1,76 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractal.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hjo <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/24 14:30:39 by hjo               #+#    #+#             */
+/*   Updated: 2018/08/24 14:30:41 by hjo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FRACTAL_H
-#define FRACTAL_H
+# define FRACTAL_H
+# include "libft.h"
+# include <math.h>
+# include <pthread.h>
+# include "../minilibx/mlx.h"
+# define WIDTH 800
+# define HEIGHT 800
 
-#include "libft.h"
-#include <stdio.h>
-#include <math.h>
-#include <pthread.h>
-#include "../minilibx/mlx.h"
-#define WIDTH 800
-#define HEIGHT 800
-
-typedef struct		s_fractal
+typedef struct	s_fractal
 {
-	void *mlx;
-	void *win;
-	void *img;
-	char *img_ptr;
+	void		*mlx;
+	void		*win;
+	void		*img;
+	char		*img_ptr;
+	int			f_type;
+	int			iter_max;
+	int			iter;
+	int			bpp;
+	int			sl;
+	int			en;
+	int			pointer_x;
+	int			pointer_y;
+	int			y_max;
+	int			color;
+	int			c_coef;
+	int			c_coeff;
+	int			n;
+	int			x;
+	int			y;
+	float		x_move;
+	float		y_move;
+	float		julia_shape;
+	float		zoom;
+	double		z;
+	double		c;
+	double		c_a;
+	double		c_b;
+}				t_fractal;
 
-	int	f_type;
-	int iter_max;
-	int iter;
-	int bpp;
-	int sl;
-	int en;
-	int pointer_x;
-	int pointer_y;
-
-	float x_move;
-	float y_move;
-	float julia_shape;
-
-	int y_max;
-	int color;
-	int c_coef;
-	int c_coeff;
-	int n;
-
-	float zoom;
-	double z;
-	double c;
-
-	double c_a;
-	double c_b;
-
-	int x;
-	int y;
-}					t_fractal;
-
-void	init_mandelbrot		(t_fractal *i);
-void	calc_mandelbrot		(t_fractal *i);
-void	*execute_mandelbrot	(void *ft);
-
-void	init_julia			(t_fractal *i);
-void	calc_julia			(t_fractal *i);
-void	*execute_julia		(void *ft);
-
-void	init_burningship	(t_fractal *i);
-void	calc_burningship	(t_fractal *i);
-void	*execute_burningship(void *ft);
-
-void	init_hydra			(t_fractal *i);
-void	calc_hydra			(t_fractal *i);
-void	*execute_hydra		(void *ft);
-
-// utility.c
-void	pixel_to_image		(t_fractal *i, int x, int y, int color);
-void	multipthread		(t_fractal *i);
-// void	pcreate				(t_fractal *ft, pthread_t *id, int c, t_fractal *i);
-void	correct_usage		(void);
-void	open_usage			(void);
-// key.c
-int		key_hook			(int key, t_fractal *i);
-int		pointer_hook		(int a, int b, t_fractal *i);
-int		mouse_hook			(int key, int x, int y, t_fractal *i);
-
+void			init_mandelbrot			(t_fractal *i);
+void			calc_mandelbrot			(t_fractal *i);
+void			*execute_mandelbrot		(void *ft);
+void			init_julia				(t_fractal *i);
+void			calc_julia				(t_fractal *i);
+void			*execute_julia			(void *ft);
+void			init_burningship		(t_fractal *i);
+void			calc_burningship		(t_fractal *i);
+void			*execute_burningship	(void *ft);
+void			init_hydra				(t_fractal *i);
+void			calc_hydra				(t_fractal *i);
+void			*execute_hydra			(void *ft);
+void			pixel_to_image			(t_fractal *i, int x, int y, int color);
+void			multipthread			(t_fractal *i);
+void			correct_usage			(void);
+void			open_usage				(void);
+int				key_hook				(int key, t_fractal *i);
+int				pointer_hook			(int a, int b, t_fractal *i);
+int				mouse_hook				(int key, int x, int y, t_fractal *i);
 #endif
--- src/utilities.c
+++ src/utilities.c
@@ -12,7 +12,7 @@
 
 #include "fractal.h"
 
-void	correct_usage(void)
+void		correct_usage(void)
 {
 	ft_putendl("Please try one of the following options");
 	ft_putendl("./fractol mandelbrot");
@@ -21,7 +21,7 @@ void	correct_usage(void)
 	ft_putendl("          hydra");
 }
 
-void	open_usage(void)
+void		open_usage(void)
 {
 	ft_putendl("====================Welcome to fractal====================");
 	ft_putendl("\t'- / +' : zoom in and out (do this on num pad)");
@@ -30,9 +30,19 @@ void	open_usage(void)
 	ft_putendl("\t'mouse' : move to change julia and hydra");
 }
 
-// void	pcreate(t_fractal *ft, pthread_t *id, int c, t_fractal *i)
+static void	pcreate(t_fractal ft[8], pthread_t id[8], int c, t_fractal *i)
+{
+	i->f_type == 1 ? pthread_create(&id[c], NULL,
+		*execute_mandelbrot, &ft[c]) : 0;
+	i->f_type == 2 ? pthread_create(&id[c], NULL,
+		*execute_julia, &ft[c]) : 0;
+	i->f_type == 3 ? pthread_create(&id[c], NULL,
+		*execute_burningship, &ft[c]) : 0;
+	i->f_type == 4 ? pthread_create(&id[c], NULL,
+		*execute_hydra, &ft[c]) : 0;
+}
 
-void	multipthread(t_fractal *i)
+void		multipthread(t_fractal *i)
 {
 	t_fractal	ft[8];
 	pthread_t	id[8];
@@ -49,24 +59,14 @@ void	multipthread(t_fractal *i)
 	c = -1;
 	while (++c < 8 && (i->f_type == 1 || i->f_type == 2 ||
 	i->f_type == 3 || i->f_type == 4))
-	{
-		i->f_type == 1 ? pthread_create(&id[c], NULL,
-			*execute_mandelbrot, &ft[c]) : 0;
-		i->f_type == 2 ? pthread_create(&id[c], NULL,
-			*execute_julia, &ft[c]) : 0;
-		i->f_type == 3 ? pthread_create(&id[c], NULL,
-			*execute_burningship, &ft[c]) : 0;
-		i->f_type == 4 ? pthread_create(&id[c], NULL,
-			*execute_hydra, &ft[c]) : 0;
-	}
-	//	pcreate(&ft[8], &id[8], c, i);
+		pcreate(ft, id, c, i);
 	c = -1;
 	while (++c < 8)
 		pthread_join(id[c], NULL);
 	mlx_put_image_to_window(i->mlx, i->win, i->img, 0, 0);
 }
 
-void	pixel_to_image(t_fractal *i, int x, int y, int color)
+void		pixel_to_image(t_fractal *i, int x, int y, int color)
 {
 	int tmp;
 
